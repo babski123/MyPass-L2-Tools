d46af7e611365af5e0f05918cb2d5d0c08ea792a
babski123 <eleazer.ababa181@gmail.com>
2023-11-15 16:43:14 +0800

    add ColorModeSwitcher

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Center, Heading, Flex, Text, Divider, VStack } from '@chakra-ui/react';
 import FileUploadForm from './components/FileUploadForm';
-//import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
 function App() {
   return (
     <Flex width={"100vw"} height={"100vh"} alignContent={"center"} justifyContent={"center"}>
       <Center flexDirection={"column"}>
+      <ColorModeSwitcher />
         <VStack spacing={3}>
           <Heading>Yotpo Bulk Unsubscriber/Resubscriber</Heading>
           <Text>This tool will add/remove email addresses from the unsubscribers list via API in batches (150 per batch).</Text>
